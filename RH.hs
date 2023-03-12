41cdb04e51bceb92bd44436c0a9030f72dd0d2e1
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2023-03-12 17:53:53 -0300

    segunda versão

--- RH.hs
+++ RH.hs
@@ -1,7 +1,11 @@
 module Main (main) where
-main :: IO ()
 
-data Cargo = Estagiario | Programador | Coordenador |Gerente deriving Show
+main :: IO ()
+main = do
+    let pessoa = Pessoa Estagiario "Joao"
+    print (verSalario pessoa)
+
+data Cargo = Estagiario | Programador | Coordenador | Gerente deriving Show
 
 data Pessoa = Pessoa {cargo :: Cargo, nome :: String} deriving Show
 
@@ -11,6 +15,11 @@ verSalario (Pessoa Programador _) = 2000
 verSalario (Pessoa Coordenador _) = 3000
 verSalario (Pessoa Gerente _) = 4000
 
-main = do
-    let pessoa = Pessoa Estagiario "Joao"
-    print (verSalario pessoa)
+verFolha :: Pessoa -> String
+verFolha p = "{nome: \"" ++ (nome p)++ "\", cargo: \"" ++ (show $ cargo p) ++ "\", salario: " ++ (show $ verSalario p) ++ "}"
+
+promover :: Pessoa -> Pessoa
+promover (Pessoa Estagiario n) = Pessoa Programador n
+promover (Pessoa Programador n) = Pessoa Coordenador n
+promover (Pessoa Coordenador n) = Pessoa Gerente n
+promover pessoa@(Pessoa Gerente _) = pessoa
