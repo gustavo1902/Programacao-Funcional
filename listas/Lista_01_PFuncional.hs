3fb9e8cd6e5cccb1eb9efcfe80c79f206c286f8c
Gustavo M. Costa <101591580+gustavo1902@users.noreply.github.com>
2023-04-04 17:39:34 -0300

    04/03

--- listas/Lista_01_PFuncional.hs
+++ listas/Lista_01_PFuncional.hs
@@ -1,3 +1,4 @@
+--1)
 f1 :: Double -> Double
 f1 x
   | x >=0 = (x+4)/(x+2)
@@ -14,11 +15,13 @@ f3 x y z
   | (x+y)<z = x-y-z
   | (x+y)==z = 0
 
+--2)
 --Claúsula de para na função fatorial
 fat :: Int->Int
 fat 0 = 1
 fat x = x * fat(x-1)
 
+--3)
 soma::Int->Int->Int
 soma x y = x + y
 
@@ -30,14 +33,22 @@ multiplica x y
 square :: Int -> Int 
 square x = x * x
 
-fourPower :: Int -> Int
-fourPower x = square (square x)
-
+--4)
 invert :: Int -> Int
 invert = read . reverse . show
 
+--5)
+fourPower :: Int -> Int
+fourPower x = square (square x)
+
+--6)
 sqtrSeq :: Int -> Double
 sqtrSeq 0 = sqrt 6
 sqtrSeq i = sqtrSeq (i-1) + sqrt 6
 
-
+--7) 
+escolha :: Int -> Int -> Int
+escolha m n
+  | n == 0 || n == m = 1
+  | n > m = 0
+  | otherwise = escolha (m-1) (n-1) + escolha (m - 1) n
