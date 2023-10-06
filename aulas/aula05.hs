d8a42022ba0cf64954056674d946bef5a0d3e6b6
Gustavo <gustavo.morais@sou.unifal-mg.edu.br>
2023-10-06 18:59:26 -0300

    feat: aula

--- aulas/aula05.hs
+++ aulas/aula05.hs
@@ -1,2 +1,6 @@
 --listas
 
+retornaNElementos :: [a] -> Int -> [a]
+retornaNElementos _ 0 = []  -- Caso base: quando n é zero, retorna uma lista vazia
+retornaNElementos [] _ = [] -- Caso base: quando a lista está vazia, retorna uma lista vazia
+retornaNElementos (x:xs) n = x : retornaNElementos xs (n-1)
