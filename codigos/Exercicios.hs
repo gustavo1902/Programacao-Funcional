291a75db61018cfd051ab6d1f24958ae17a404a1
gustavo1902 <gustavomorais1009@gmail.com>
2023-10-27 17:02:56 -0300

    listas

--- codigos/Exercicios.hs
+++ codigos/Exercicios.hs
@@ -8,4 +8,12 @@ listaReversa (x:xs) = listaReversa xs ++ [x]
 somaImpares :: [Int] -> Int
 somaImpares [] = 0
 somaImpares (x:xs)
-    |
+    | odd x = x + somaImpares xs
+    | otherwise = somaImpares xs
+
+
+somaPares :: [Int] -> Int
+somaPares []= 0
+somaPares (x:xs)
+    | even x = x + somaPares xs
+    | otherwise = somaPares xs
