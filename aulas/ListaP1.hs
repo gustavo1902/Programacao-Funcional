1c9b7e1a7f0885cd4fa39dd82d6f8376a1952519
Gustavo <gustavo.morais@sou.unifal-mg.edu.br>
2023-11-01 14:45:09 -0300

    fix: Questão 3 e 4

---
+++ aulas/ListaP1.hs
@@ -0,0 +1,36 @@
+--1
+ehPrimo :: Int -> Bool
+ehPrimo x 
+    | mod x x ==0 && mod x 1 == 0 = True
+    | otherwise = False
+
+--2
+ordenaEmTupla :: Int -> Int -> Int -> Int -> (Int, Int, Int, Int)
+ordenaEmTupla a b c d = (min4 a b c d, min3 a b c d, min2 a b c d, max4 a b c d)
+
+min2 :: Int -> Int -> Int -> Int -> Int
+min2 a b c d = min a (min b (min c d))
+
+min3 :: Int -> Int -> Int -> Int -> Int
+min3 a b c d = min a (min b (min c (min d a)))
+
+min4 :: Int -> Int -> Int -> Int -> Int
+min4 a b c d = min a (min b (min c d))
+
+max4 :: Int -> Int -> Int -> Int -> Int
+max4 a b c d = max a (max b (max c d))
+
+--3
+quantosDias :: Int -> Int
+quantosDias x
+    | mod x 4 == 0 = 366
+    | otherwise = 365 
+
+--4
+diasMes :: Int -> Int -> Int
+diasMes ano mes
+    | elem mes [4, 6, 9, 11] = 30
+    | mes == 2 = if anoBissexto then 29 else 28
+    | otherwise = 31
+    where anoBissexto = (mod ano 4 == 0)
+
